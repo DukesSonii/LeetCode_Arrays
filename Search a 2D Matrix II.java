bd1d67ca7ac329a62a8cb5c4df5a0ce155aa686e
Duke soni <dukesoni05@gmail.com>
2023-02-12 14:39:37 +0530

    Create Search a 2D Matrix II.java

---
+++ Search a 2D Matrix II.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) { 
+        int n = matrix.length;
+        int row = 0, col = matrix[0].length - 1;
+        while(row < n && col >= 0){
+            if(matrix[row][col] == target){ 
+                return true;
+            }
+            else if(matrix[row][col] < target)
+            {
+                row++;
+            }
+            else
+                col--;
+        }
+        return false;
+    }
+}
+
