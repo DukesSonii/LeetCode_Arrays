b86ea14cd49cfe9a64a999b894dfbc15ea954c88
Duke soni <dukesoni05@gmail.com>
2023-02-15 16:37:42 +0530

    Create contains duplicate.java

---
+++ contains duplicate.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(nums[i])) {
+                int idx = map.get(nums[i]);
+                if (i - idx <= k) {
+                    return true;
+                }
+            }
+            map.put(nums[i], i);
+        }
+
+        return false;
+    }
+}
